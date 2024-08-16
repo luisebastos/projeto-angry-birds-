f0819e6a095161ef13cd3cee0dc4445c8261eae8
saragocams <saragocams@gmail.com>
2024-08-15 21:39:09 -0300

    adicionei tela de menu e criei a imagem do joguinho e aumentei a tela (precisa ver um jeito para deixar o menu menos pixelado)

--- jogo_real.py
+++ jogo_real.py
@@ -1,13 +1,15 @@
 import pygame
 
 class SimpleScreen:
-    def __init__(self, width=800, height=450):
+    def __init__(self, width=900, height=500):
         pygame.init()
         pygame.display.set_caption('Angry Birds')
+        
         
         # Configurações da tela
         self.screen = pygame.display.set_mode((width, height))
         self.BLACK = (0, 0, 0)
+        self.start_screen_image = pygame.image.load("assets/menu.png")
         
         self.rodando = True
 
@@ -17,7 +19,7 @@ class SimpleScreen:
                 self.rodando = False
 
     def desenhar(self):
-        self.screen.fill(self.BLACK)
+        self.screen.blit(self.start_screen_image, (0, 0))
         pygame.display.update()
 
     def rodar(self):
